10f0330b98711ed1920bf50b7b98df1827472073
Andrew Wilson <drbobchoco@gmail.com>
2014-04-26 15:15:22 +0100

    added getName()

--- mailConfigBlank.js
+++ mailConfigBlank.js
@@ -2,9 +2,12 @@
 module.exports.getUser = function() {
 	return "user";
 }
-module.exports.getAddress = function() {
-	return "address";
-}
 module.exports.getPassword = function() {
 	return "password";
+}
+module.exports.getName = function() {
+	return "Name";
+}
+module.exports.getAddress = function() {
+	return "address";
 }
